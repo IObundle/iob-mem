79632d5defe18bbc26a89d740087951e3b8d1c14
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-04-15 04:05:26 +0100

    add true dual port memory

--- tdp_mem/iob_tdp_mem.v
+++ tdp_mem/iob_tdp_mem.v
@@ -1,54 +1,38 @@
-// True-Dual-Port BRAM with Byte-wide Write Enable
-// Read-First mode 
-
 `timescale 1 ns / 1 ps
 
-module bytewrite_tdp_ram_rf 
-  #(parameter NUM_COL = 4,
-    parameter COL_WIDTH = 8,
-    parameter ADDR_WIDTH = 10,  
-    //Addr Width in bits : 2*ADDR_WIDTH = RAM Depth
-    parameter DATA_WIDTH = NUM_COL*COL_WIDTH  //Data Width in bits
-    ) 
-   ( 
-     input                        clkA, 
-     input                        enaA, 
-     input [NUM_COL-1:0]          weA, 
-     input [ADDR_WIDTH-1:0]       addrA, 
-     input [DATA_WIDTH-1:0]       dinA, 
-     output reg [DATA_WIDTH-1:0]  doutA, 
-     input                        clkB, 
-     input                        enaB,
-     input [NUM_COL-1:0]          weB, 
-     input [ADDR_WIDTH-1:0]       addrB, 
-     input [DATA_WIDTH-1:0]       dinB, 
-     output reg [DATA_WIDTH-1 :0] doutB 
-     ); 
+module iob_t2p_mem #(
+		      parameter MEM_INIT_FILE=0,
+		      parameter DATA_W=32,
+		      parameter ADDR_W=11
+		      )
+   (
+    input [(DATA_W-1):0] 	  data_a, data_b,
+    input [(ADDR_W-1):0] 	  addr_a, addr_b,
+    input 			  en_a, en_b, we_a, we_b, clk,
+    output reg [(DATA_W-1):0] q_a, q_b
+		    );
 
+   // Declare the RAM
+   reg [DATA_W-1:0] 			       ram[2**ADDR_W-1:0];
 
-   // Core Memory 
-   reg [DATA_WIDTH-1:0]           ram_block[(2**ADDR_WIDTH)-1:0]; 
+   // Initialize the RAM
+   //initial $readmemh(MEM_INIT_FILE, ram, 0, 2**ADDR_W - 1);
 
-   integer                        i;  
-   
-   //Port-A Operation 
-   always @ (posedge clkA) begin 
-      if(enaA) begin 
-         for(i=0;i<NUM_COL;i=i+1) begin 
-            if(weA[i]) begin 
-               ram_block[addrA][i*COL_WIDTH +: COL_WIDTH] <= dinA[i*COL_WIDTH +: COL_WIDTH]; 
-            end 
-         end doutA <= ram_block[addrA]; //Send Feedback
-      end    
-   end
-   
-   //Port-B Operation: 
-   always @ (posedge clkB) begin 
-      if(enaB) begin
-         for(i=0;i<NUM_COL;i=i+1) begin 
-            if(weB[i]) begin
-               ram_block[addrB][i*COL_WIDTH +: COL_WIDTH] <= dinB[i*COL_WIDTH +: COL_WIDTH]; end end doutB <= ram_block[addrB]; 
-      end
-   end
-   
-endmodule // bytewrite_tdp_ram_rf
+   // Operate the RAM
+   always @ (posedge clk) // Port A
+     if(en_a)
+       if (we_a) begin
+	  ram[addr_a] <= data_a;
+	  q_a <= data_a;
+       end else
+	 q_a <= ram[addr_a];
+
+   always @ (posedge clk) // Port b
+     if(en_b)
+       if (we_b) begin
+	  ram[addr_b] <= data_b;
+	  q_b <= data_b;
+       end else
+	 q_b <= ram[addr_b];
+
+ endmodule
