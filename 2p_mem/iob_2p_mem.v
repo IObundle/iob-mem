ce42f38b3c8051866834e0b7ced84523390b5ad7
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-05-19 15:22:55 +0100

    fix 2p mem

--- 2p_mem/iob_2p_mem.v
+++ 2p_mem/iob_2p_mem.v
@@ -1,51 +1,42 @@
 `timescale 1ns/1ps
 
 module iob_2p_mem
-	#( 
-		parameter DATA_W = 8,
-		parameter ADDR_W = 6,
-		parameter USE_RAM = 1
-	) 
-	(
-	 input                   clk,
+  #( 
+     parameter DATA_W = 8,
+     parameter ADDR_W = 6,
+     parameter USE_RAM = 1
+     ) 
+   (
+    input                   clk,
 
-	 //write port
-         input                   w_port_en,
-         input                   w_en,
-         input [ADDR_W-1:0]      w_addr,
-         input [DATA_W-1:0]      data_in,
+    //write port
+    input                   w_en,
+    input [ADDR_W-1:0]      w_addr,
+    input [DATA_W-1:0]      data_in,
 
-         //read port
-         input                   r_port_en,
-         input [ADDR_W-1:0]      r_addr,
-         output reg [DATA_W-1:0] data_out
+    //read port
+    input                   r_en,
+    input [ADDR_W-1:0]      r_addr,
+    output reg [DATA_W-1:0] data_out
     );
 
    //memory declaration
-   generate
-   if(USE_RAM) begin
-	reg [DATA_W-1:0]              ram [2**ADDR_W-1:0];
-   //writing to the RAM
-    always@(posedge clk)
-	  if(w_en && w_port_en)
-	    ram[w_addr] <= data_in;
-   //reading from the RAM
-   always@(posedge clk)
-     if(r_port_en)
-       data_out <= ram[r_addr];
-   end
-   else begin
-	reg [DATA_W*(2**ADDR_W)-1:0] 	 mem ;
-   //writing to the register
-	always@(posedge clk)
-   	  if(w_en && w_port_en)
-	    mem[8*w_addr +: 8] <= data_in;
-	//reading from the RAM
-    always@(posedge clk)
-      if(r_port_en)
-        data_out <= mem[8*r_addr +: 8];
-   end
-   endgenerate
+   reg [DATA_W-1:0]         mem [2**ADDR_W-1:0];
 
+   //write
+   always@(posedge clk)
+     if(w_en)
+       ram[w_addr] <= data_in;
+
+   //read mode depends on mem implementation, as ram or reg
+   generate
+      if(USE_RAM)
+        always@(posedge clk)  begin
+           if(r_en)
+             data_out <= ram[r_addr];
+        end
+      else //use reg file 
+        data_out <= mem[r_addr];
+   endgenerate
 
 endmodule   
