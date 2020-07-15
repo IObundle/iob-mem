af6af3b334f5256b3bb066f97136cf1e32fd371e
Chris Edgley <christopher@iobundle.com>
2020-07-15 18:25:41 +0100

    name change: MEMGET/SET -> IO_GET/SET

--- fifo/software/fifo.c
+++ fifo/software/fifo.c
@@ -2,30 +2,30 @@
 #include "interconnect.h"
 
 int fifo_empty(int base) {
-  return MEMGET(base, FIFO_EMPTY);
+  return IO_GET(base, FIFO_EMPTY);
 }
 
 int fifo_full(int base) {
-  return MEMGET(base, FIFO_FULL);
+  return IO_GET(base, FIFO_FULL);
 }
 
 int fifo_read(int base) {
-  return MEMGET(base, FIFO_DATA);
+  return IO_GET(base, FIFO_DATA);
 }
 void fifo_write(int base, int word) {
-    MEMSET(base, FIFO_DATA, word);
+    IO_SET(base, FIFO_DATA, word);
 }
 
 void fifo_flush(int base) {
-  MEMSET(base, FIFO_FLUSH, 0x0);
+  IO_SET(base, FIFO_FLUSH, 0x0);
 }
 
 int fifo_level_w(int base) {
-  return MEMGET(base, FIFO_LEVEL_W);
+  return IO_GET(base, FIFO_LEVEL_W);
 }
 
 int fifo_level_r(int base) {
-  return MEMGET(base, FIFO_LEVEL_R);
+  return IO_GET(base, FIFO_LEVEL_R);
 }
 
 unsigned int fifo_tx_samples2full(int base, int depth) {
