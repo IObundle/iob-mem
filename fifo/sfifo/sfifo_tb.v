8b5ebe6ece81eeb9a0e8e8172a25ea1268861cb4
Jose Sarmento <jose.j.sarmento@me.com>
2020-08-10 16:15:30 +0100

    tb changes, sfifo make, readme, removed log2 (#15)
    * tb changes, sfifo make, removed log2 from .v
    
    * readme change

--- fifo/sfifo/sfifo_tb.v
+++ fifo/sfifo/sfifo_tb.v
@@ -3,7 +3,7 @@
 module sfifo_tb;
 	
 	//Inputs
-	reg clk;
+	  reg clk;
     reg reset;
    	reg [7:0] data_in;
    	reg read;
@@ -15,14 +15,18 @@ module sfifo_tb;
    	wire full_out;
 
     integer i;
+
+    parameter clk_per = 10; // clk period = 10 timeticks
     
 
     initial begin
-    
-    	$dumpfile("sfifo.vcd");
-    	$dumpvars();
-    	
-    	//Initialize Inputs
+        // optional VCD
+        `ifdef VCD
+      	   $dumpfile("sfifo.vcd");
+      	   $dumpvars();
+        `endif
+      	
+        //Initialize Inputs
         clk = 1;
         reset = 0;
         data_in = 0;
@@ -30,7 +34,7 @@ module sfifo_tb;
         write = 0;
         
          //Write all the locations of FIFO
-   		#10;
+        #10;
         @(posedge clk) #1; 
         reset = 1;
         @(posedge clk) #1;
@@ -38,26 +42,35 @@ module sfifo_tb;
         
         @(posedge clk) #1;
        
-		for(i=1; i <= 16; i = i + 1) begin
-        	 write = 1;
-			data_in = i;
-			@(posedge clk) #1;
-		end
+        for(i=1; i <= 16; i = i + 1) begin
+             write = 1;
+            data_in = i;
+            if ( data_in != i ) begin
+                $display("Test failed on vector %d: %x / %x", i, read, i);
+                $finish;
+            end
+            @(posedge clk) #1;
+        end
        
-       @(posedge clk) #1;
-       write = 0; //Fifo is now full
+        @(posedge clk) #1;
+        write = 0; //Fifo is now full
         
         #10
         @(posedge clk) #1;
         read=1;
         //Read all the locations of RAM. 
-		for(i=1; i <= 16; i = i + 1) begin
-			//Result will only be available in the next cycle
-			@(posedge clk) #1;
-		end
-		@(posedge clk) #1;
-		read = 0; //Fifo is now empty
-        #50 $finish;
+        for(i=1; i <= 16; i = i + 1) begin
+            //Result will only be available in the next cycle
+            @(posedge clk) #1;
+        end
+
+        @(posedge clk) #1;
+        read = 0; //Fifo is now empty
+
+        #clk_per
+        $display("Test completed successfully");
+        #(5*clk_per) $finish;
+
     end
 
    	// Instantiate the Unit Under Test (UUT)
@@ -76,7 +89,7 @@ module sfifo_tb;
 		.write_en(write)
 	);
     
-	always #5 clk = ~clk; 
+	always #(clk_per/2) clk = ~clk; 
 
 endmodule // sfifo_tb
 
