a33550d8c4bb3c8f9941a751f4619037b46447aa
Dr-Specimen <jose.j.sarmento@me.com>
2020-08-11 12:26:29 +0100

    updated 2p_mem make and tb

--- 2p_mem/iob_2p_mem_tb.v
+++ 2p_mem/iob_2p_mem_tb.v
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
 
-`define T 10
-
 `define DATA_W 8
 `define ADDR_W 4
 
@@ -13,67 +11,67 @@ module iob_2p_mem_tb;
 
 
    //write signals
-   reg w_port_en;
    reg w_en;
    reg [`DATA_W-1:0] data_in;
    reg [`ADDR_W-1:0] w_addr;
 
 
    //read signals   
+   reg r_en;
    reg [`ADDR_W-1:0] r_addr;
-   reg               r_port_en;
    wire [`DATA_W-1:0] data_out;
    
 
    integer            i=0;
 
+   parameter clk_per = 10; // clk period = 10 timeticks
+
    initial begin
-      $dumpfile("2p_mem_tb.vcd");
-      $dumpvars();
+        // optional VCD
+        `ifdef VCD
+        $dumpfile("sfifo.vcd");
+        $dumpvars();
+        `endif
 
-      clk = 1;
-      w_port_en = 0; 
-      w_en = 0;
-      w_addr = 0;
-      data_in = 0;
+        clk = 1;
+        w_en = 0;
+        w_addr = 0;
+        data_in = 0;
 
-      r_port_en = 0;
-      r_addr = 0;
+        r_addr = 0;
 
-      @(posedge clk) #1;
-      w_port_en = 1; 
-      w_en = 1;
+        @(posedge clk) #1;
+        w_en = 1;
 
-      //Write all the locations of RAM 
-      for(i=0; i < 16; i = i + 1) begin
-	 data_in = i;
-	 w_addr = i;
-	 @(posedge clk) #1;
-      end
+        //Write all the locations of RAM 
+        for(i=0; i < 16; i = i + 1) begin
+            data_in = i;
+            w_addr = i;
+            if(data_in!=i) begin
+                $display("Test failed on vector %d: %x / %x", i, data_in, i);
+                $finish;
+            end
+            @(posedge clk) #1;
+        end
 
-      w_port_en = 0;
-      w_en = 0; 	 
-      r_port_en = 1; 
- 
-      //Read all the locations of RAM
-      for(i=0; i < 16; i = i + 1) begin
-         r_addr = i;
-	 @(posedge clk) #1;
-      end
+        w_en = 0; 	 
+        //r_port_en = 1; 
 
-      // Test
-      for(i=0; i < 16; i = i + 1) begin
-         if(data_in!=i) begin
-           $display("Test failed on vector %d: %x / %x", i, data_in, i);
-           $finish;
-         end
-      @(posedge clk) #1;
-      end
-      #(5*T)
-      $display("Test completed sucessfully.")
-      $finish;
+        //Read all the locations of RAM
+        for(i=0; i < 16; i = i + 1) begin
+            r_addr = i;
+            if(r_addr!=i) begin
+                $display("Test failed on vector %d: %x / %x", i, r_addr, i);
+                $finish;
+            end
+            @(posedge clk) #1;
+        end
 
-   end
+        #(5*clk_per);
+        $display("Test completed sucessfully.");
+        $finish;
+
+    end
 
     // Instantiate the Unit Under Test (UUT)
     iob_2p_mem #(
@@ -83,16 +81,15 @@ module iob_2p_mem_tb;
     )
      uut (
         .clk(clk), 
-        .w_en(w_en), 
+        .w_en(w_en),
+        .r_en(r_en), 
         .data_in(data_in), 
         .w_addr(w_addr), 
         .r_addr(r_addr),
-        .w_port_en(w_port_en),
-        .r_port_en(r_port_en),
         .data_out(data_out)
         );
 
    //Clock
-   always #(T/2) clk = ~clk;
+   always #(clk_per/2) clk = ~clk;
 
 endmodule
