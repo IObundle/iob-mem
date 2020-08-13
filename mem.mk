e1d6acc618f1cda32cdf0912389e17eb4f7b2678
Dr-Specimen <jose.j.sarmento@me.com>
2020-08-13 16:20:55 +0100

    added waves target

--- mem.mk
+++ mem.mk
@@ -11,3 +11,8 @@ CMPLR := $(VLOG) $(VSRC)
 # hex files generation for tb
 GEN_HEX1 := echo "!IObundle 2020!" | od -A n -t x1 > tb1.hex
 GEN_HEX2 := echo "10 9 8 7 5 4 32" | od -A n -t x1 > tb2.hex
+
+all: run
+
+waves: run
+	gtkwave *.vcd
