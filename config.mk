e491ab2efb827dcbecf9a18b1191b99d61f240ad
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-01-16 16:35:15 +0000

    add readme

--- config.mk
+++ config.mk
@@ -1,9 +1,3 @@
-# generate .vcd file by default
-VCD ?=1
-
-# optional ram
-USE_RAM ?=1
-
 #
 # Paths
 #
@@ -12,5 +6,12 @@ MEM_HW_DIR=$(MEM_DIR)/hardware
 MEM_SW_DIR=$(MEM_DIR)/software
 MEM_PYTHON_DIR=$(SW_DIR)/python
 
+
+#SIMULATION
+
 # Default module to simulate
 MODULE_DIR ?= $(MEM_HW_DIR)/ram/sp_ram
+
+# generate .vcd file by default
+VCD ?=1
+
