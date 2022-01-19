ea0263dadd5cbd412028f189ebc71c667694f68d
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-01-19 23:33:49 +0000

    fix typos

--- hardware/regfile/iob_regfile_dp/hardware.mk
+++ hardware/regfile/iob_regfile_dp/hardware.mk
@@ -1,14 +1,14 @@
 ifneq ($(ASIC),1)
-ifneq (iob_reg_file_dp,$(filter iob_reg_file_dp,, $(MODULES)))
+ifneq (iob_regfile_dp,$(filter iob_regfile_dp,, $(MODULES)))
 
 # Add to modules list
-MODULES+=iob_reg_file_dp
+MODULES+=iob_regfile_dp
 
 # Paths
-DP_REGFILE_DIR=$(MEM_REGF_DIR)/iob_reg_file_dp
+DP_REGFILE_DIR=$(MEM_REGF_DIR)/iob_regfile_dp
 
 # Sources
-VSRC+=$(DP_REGFILE_DIR)/iob_reg_file_dp.v
+VSRC+=$(DP_REGFILE_DIR)/iob_regfile_dp.v
 
 endif
 endif
--- hardware/regfile/iob_regfile_sp/hardware.mk
+++ hardware/regfile/iob_regfile_sp/hardware.mk
@@ -1,14 +1,14 @@
 ifneq ($(ASIC),1)
-ifneq (iob_reg_file_sp,$(filter iob_reg_file_sp,, $(MODULES)))
+ifneq (iob_regfile_sp,$(filter iob_regfile_sp,, $(MODULES)))
 
 # Add to modules list
-MODULES+=iob_reg_file_sp
+MODULES+=iob_regfile_sp
 
 # Paths
-SP_REGFILE_DIR=$(MEM_REGF_DIR)/iob_reg_file_sp
+SP_REGFILE_DIR=$(MEM_REGF_DIR)/iob_regfile_sp
 
 # Sources
-VSRC+=$(SP_REGFILE_DIR)/iob_reg_file_sp.v
+VSRC+=$(SP_REGFILE_DIR)/iob_regfile_sp.v
 
 endif
 endif
