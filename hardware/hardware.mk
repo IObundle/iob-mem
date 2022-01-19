4d1d3d2012b12868cfb8a8131a7c76acfba6b415
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-01-19 21:46:10 +0000

    improve makefile macro name

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,10 +1,10 @@
 include $(MEM_DIR)/config.mk
 
-define GET_MEM_MODULES_DIRS
+define GET_MEM_MODULE_DIR
 $(shell find $(MEM_HW_DIR) -name $(1))
 endef
 
 #include memory modules
-$(foreach p, $(MEM_MODULES), $(if $(filter $p, $(MODULES)), , $(eval MEM_MODULES_DIRS+=$(call GET_MEM_MODULES_DIRS, $p))))
+$(foreach p, $(MEM_MODULES), $(if $(filter $p, $(MODULES)), , $(eval MEM_MODULES_DIRS+=$(call GET_MEM_MODULE_DIR, $p))))
 $(foreach p, $(MEM_MODULES_DIRS), $(eval include $p/hardware.mk))
 
