9680d5a88c2310c2df35b50ec08358de5766a05e
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-01-24 00:32:58 +0000

    simplify hardware.mk

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,11 +1,6 @@
 include $(MEM_DIR)/config.mk
 
-define GET_MEM_MODULE_DIR
-$(shell find $(MEM_HW_DIR) -name $(1))
-endef
-
 #include memory modules
-#MEM_MODULES_DIRS=""
-$(foreach p, $(MEM_MODULES), $(eval MEM_MODULES_DIRS+=$(call GET_MEM_MODULE_DIR, $p)))
+$(foreach p, $(MEM_MODULES), $(eval MEM_MODULES_DIRS+=$(shell find $(MEM_HW_DIR) -name $p)))
 $(foreach p, $(MEM_MODULES_DIRS), $(eval include $p/hardware.mk))
 
